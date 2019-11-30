7ec8048957e618625f3ec802a740a3e45161c595
IkedaMika <bivi.san@gmail.com>
2019-11-30 14:04:29 +0900

    グループ編集機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
   def edit
   end
 
@@ -12,9 +15,8 @@ class UsersController < ApplicationController
 end
 
 private
-
 def user_params
-  params.require(:user).parmit(:name,:email)
+  params.require(:user).permit(:name, :email)
 end
 
 end
