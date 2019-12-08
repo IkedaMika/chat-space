54c307305c2957cdad97e7018b2ca75b501c6381
IkedaMika <bivi.san@gmail.com>
2019-12-08 19:55:29 +0900

    インクリメンタルサーチ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-    respond_to do |format|
-      format,html
-      format,json
-    end
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+   respond_to do |format|
+    format.html
+    format.json
+      end
   end
 
   def edit
@@ -16,11 +18,10 @@ class UsersController < ApplicationController
     else
       render :edit
     end
-end
+  end
 
-private
-def user_params
-  params.require(:user).permit(:name, :email)
-end
-
+  private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
